b756b6fac5e6b3a3319f9c1298cd5b6b28a5ab57
RaHaTex261 <you@example.com>
2025-02-09 03:18:12 +0400

    Initial commit

---
+++ README.txt
@@ -0,0 +1,2 @@
+Ce projet est crée par vite+react 
+pour le lancer npm run dev
---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="p-2 border-t-2 border-gray-200">
+      <p className="text-sm text-center uppercase">Boxy -  © 2025 RaHaTex - Tous droits réservés.©</p>
+    </footer>
+  )
+}
